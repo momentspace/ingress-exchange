a5e92f91d3ac4fd1c344eda4b96492c9dfaf0b21
momentspace <service@starferry.net>
2015-06-15 02:00:26 +0900

    change bootstrap to bootswatch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150614155729) do
+ActiveRecord::Schema.define(version: 20150614160620) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trade_items", force: :cascade do |t|
+    t.integer  "number"
+    t.integer  "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "trades", force: :cascade do |t|
     t.string   "name"
